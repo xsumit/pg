7e2f4bbcf1adc546f77f8102e5f48549a68de8ed
Sumit Kumar <sumit.kumar@athenalogics.com>
2019-10-02 16:40:25 +0530

    start

---
+++ www/js/app.js
@@ -0,0 +1,103 @@
+var $$ = Dom7;
+
+var app = new Framework7({
+  root: '#app', // App root element
+  id: 'io.framework7.myapp', // App bundle ID
+  name: 'My App', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+      // Demo products for Catalog section
+      products: [
+        {
+          id: '1',
+          title: 'Apple iPhone 8',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+        },
+        {
+          id: '2',
+          title: 'Apple iPhone 8 Plus',
+          description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+        },
+        {
+          id: '3',
+          title: 'Apple iPhone X',
+          description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+        },
+      ]
+    };
+  },
+  // App root methods
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  // App routes
+  routes: routes,
+
+
+
+  // Input settings
+  input: {
+    scrollIntoViewOnFocus: Framework7.device.cordova && !Framework7.device.electron,
+    scrollIntoViewCentered: Framework7.device.cordova && !Framework7.device.electron,
+  },
+  // Cordova Statusbar settings
+  statusbar: {
+    overlay: Framework7.device.cordova && Framework7.device.ios || 'auto',
+    iosOverlaysWebView: true,
+    androidOverlaysWebView: false,
+  },
+  on: {
+    init: function () {
+      var f7 = this;
+      if (f7.device.cordova) {
+        // Init cordova APIs (see cordova-app.js)
+        cordovaApp.init(f7);
+      }
+
+    },
+  },
+});
+
+// Login Screen Demo
+$$('#my-login-screen .login-button').on('click', function () {
+  var usernamee = $$('#my-login-screen [name="username"]').val();
+  var password = $$('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+});
+
+
+/*=== With Captions ===*/
+var myPhotoBrowserPopupDark = app.photoBrowser.create({
+  photos : [
+      {
+          url: 'https://cdn.framework7.io/placeholder/sports-1024x1024-1.jpg',
+          caption: 'Caption 1 Text'
+      },
+      {
+          url: 'https://cdn.framework7.io/placeholder/sports-1024x1024-2.jpg',
+          caption: 'Second Caption Text'
+      },
+      // This one without caption
+      {
+          url: 'https://cdn.framework7.io/placeholder/sports-1024x1024-3.jpg',
+      },
+  ],
+  theme: 'dark',
+  type: 'standalone'
+});
+
+ myPhotoBrowserPopupDark.open();
+
